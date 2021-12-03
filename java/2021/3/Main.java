738661b2818590421b3a32cfc9b7882d4cf2c5ae
Mattia Lunardi <mattia.lunardi@gmail.com>
2021-12-03 23:21:35 +0100

    Completed day 3 2021
    Refactorization needed

--- java/2021/3/Main.java
+++ java/2021/3/Main.java
@@ -27,6 +27,13 @@ public class Main{
 		return res;
 	}
 
+	private static char[][] convertToBits(LinkedList<String> diagnosticReport){
+		char[][] bits = new char[diagnosticReport.size()][diagnosticReport.get(0).length()];
+		for(int i = 0; i < diagnosticReport.size(); i++)
+			bits[i] = diagnosticReport.get(i).toCharArray();
+		return bits;
+	}
+
 	private static long convertToDecimal(int[] binaryNumbers){
 		long sum = 0;
 		int pow = binaryNumbers.length - 1;
@@ -37,110 +44,212 @@ public class Main{
 		return sum;
 	}
 
-
-	private static long findGammaRate(LinkedList<String> diagnosticReport){
-		
+	private static int[] countZeroesAndOnes(String criteria, char[][] bits){
 		int count_zero = 0;
 		int count_one = 0;
-
-		char[][] bits = new char[diagnosticReport.size()][diagnosticReport.get(0).length()];
-		int[] counters = new int[diagnosticReport.get(0).length()];
-		
-		for(int i = 0; i < diagnosticReport.size(); i++){
-			char[] tmp = diagnosticReport.get(i).toCharArray();
-			bits[i] = tmp;
-		}
 		int saveIndex = 0;
 		int j = 0;
 		int curRow = 0;
-		int stop = bits.length * bits[0].length;
-		for(int i = 0; i < stop; i++){
+		int stopLimit = bits.length * bits[0].length;
+		int[] res = new int[bits[0].length];
+
+		for(int i = 0; i < stopLimit; i++){
 			if(bits[curRow][j] == '0')
 				count_zero++;
 			else
 				count_one++;
-			if(curRow == bits.length - 1){
+			if(curRow == bits.length -1){
 				curRow = 0;
-				if(count_zero > count_one){
-					counters[saveIndex] = 0;
-					saveIndex++;
+				if(criteria.equals("MSB")){
+					if(count_zero > count_one){
+						res[saveIndex] = 0;
+						saveIndex++;
+					}
+					else{
+						res[saveIndex] = 1;
+						saveIndex++;
+					}
+					j++;
+					count_zero = 0;
+					count_one = 0;
+					if(j == bits[curRow].length)
+						j = 0;
 				}
-				else{
-					counters[saveIndex] = 1;
-					saveIndex++;
+				else if(criteria.equals("LSB")){
+					if(count_zero < count_one){
+						res[saveIndex] = 0;
+						saveIndex++;
+					}
+					else{
+						res[saveIndex] = 1;
+						saveIndex++;
+					}
+					j++;
+					count_zero = 0;
+					count_one = 0;
+					if(j == bits[curRow].length)
+						j = 0;
 				}
-				j++;
-				count_zero = 0;
-				count_one = 0;
-				
-				if(j == bits[curRow].length)
-					j = 0;
 			}
 			else
-				curRow++;				
+				curRow++;
 		}
-		
-		return convertToDecimal(counters);
+		return res;
 	}
 
-	private static long findEpsilonRate(LinkedList<String> diagnosticReport){
+	/**
+	 * Part one methods
+	 * **/
+
+
+	private static long findGammaRate(char[][] bits){
+		int[] bin = countZeroesAndOnes("MSB", bits);
+		return convertToDecimal(bin);
+	}
+
+	private static long findEpsilonRate(char[][] bits){
+		int[] bin = countZeroesAndOnes("LSB", bits);
+		return convertToDecimal(bin);
+	}
+
+	/**
+	 * Part two methods
+	 * */
+
+	private static char[][] mostCommonValues(int pos, String criteria, char[][] bits){
 		int count_zero = 0;
 		int count_one = 0;
 
-		char[][] bits = new char[diagnosticReport.size()][diagnosticReport.get(0).length()];
-		int[] counters = new int[diagnosticReport.get(0).length()];
 		
-		for(int i = 0; i < diagnosticReport.size(); i++){
-			char[] tmp = diagnosticReport.get(i).toCharArray();
-			bits[i] = tmp;
-		}
-		int saveIndex = 0;
-		int j = 0;
-		int curRow = 0;
-		int stop = bits.length * bits[0].length;
+		char[][] res;
+		LinkedList<String> tmp = new LinkedList<String>();
+		LinkedList<String> zeroes = new LinkedList<String>();
+		LinkedList<String> ones = new LinkedList<String>();
 
-		for(int i = 0; i < stop; i++){
-			if(bits[curRow][j] == '0')
+		for(int i = 0; i < bits.length; i++){
+			if(bits[i][pos] == '0'){
 				count_zero++;
-			else
-				count_one++;
-			if(curRow == bits.length - 1){
-				curRow = 0;
-				if(count_zero < count_one){
-					counters[saveIndex] = 0;
-					saveIndex++;
-				}
-				else{
-					counters[saveIndex] = 1;
-					saveIndex++;
-				}
-				j++;
-				count_zero = 0;
-				count_one = 0;
-				
-				if(j == bits[curRow].length)
-					j = 0;
+				zeroes.add(new String(bits[i]));
 			}
-			else
-				curRow++;				
-		}	
+			else{
+				count_one++;
+				ones.add(new String(bits[i]));
+			}
+			if(i == bits.length - 1){
+				if(criteria.equals("MCV")){
+					if(count_zero > count_one){
+						for(int j = 0; j < zeroes.size(); j++)
+							tmp.add(zeroes.get(j));
+					}
+					else if(count_zero ==  count_one){
+						for(int j = 0; j < ones.size(); j++)
+							tmp.add(ones.get(j));
+					}
+					else{
+						for(int j = 0; j < ones.size(); j++)
+							tmp.add(ones.get(j));
+					}
+				}
+				else if(criteria.equals("LCV")){
+					if(count_zero < count_one){
+						for(int j = 0; j < zeroes.size(); j++)
+							tmp.add(zeroes.get(j));
+					}
+					else if(count_zero == count_one){
+						for(int j = 0; j < zeroes.size(); j++)
+							tmp.add(zeroes.get(j));	
+					}
+					else{
+						for(int j = 0; j < ones.size(); j++)
+							tmp.add(ones.get(j));
+					}
+				}
+			}
+		}
+		res = new char[tmp.size()][tmp.get(0).length()];
+		for(int i = 0; i < tmp.size(); i++)
+			res[i] = tmp.get(i).toCharArray();
+		
+		return res;
+	}
 
-		return convertToDecimal(counters);
+	/**
+	 * Determine the most common value in the current position,
+	 * keep only the numbers with that bit in that position. if 0 and 1
+	 * are equally common, keep values with a 1 in the position being
+	 * considered
+	 * */
+	private static long findOxigenRating(char[][] bits){
+		int pos = 0;
+		int numbersLeft = bits.length;
+
+		while(numbersLeft > 1){
+			bits = mostCommonValues(pos, "MCV", bits);
+			numbersLeft = bits.length;
+			pos++;
+			if(pos == bits[0].length)
+				pos = 0;
+			//System.out.println("numbers left in bits: "+numbersLeft);
+			//System.out.println("----------------------------");
+		}
+
+		char[] tmp = bits[0];
+		int[] resBin = new int[tmp.length];
+		for(int i = 0; i < tmp.length; i++)
+			resBin[i] = Character.getNumericValue(tmp[i]);
+
+		return convertToDecimal(resBin);
+	}
+
+	/**
+	 * Determine the least common value in the current position,
+	 * keep only the numbers with that bit in that position. if 0 or 1
+	 * are equally common, keep values with a 0 in the position being 
+	 * considered
+	 * */
+	private static long findScrubberRating(char[][] bits){
+		int pos = 0;
+		int numbersLeft = bits.length;
+
+		while(numbersLeft > 1){
+			bits = mostCommonValues(pos, "LCV", bits);
+			numbersLeft = bits.length;
+			pos++;
+			if(pos == bits[0].length)
+				pos = 0;
+			//System.out.println("numbers left in bits: "+numbersLeft);
+			//System.out.println("----------------------------");
+		}
+
+		char[] tmp = bits[0];
+		int[] resBin = new int[tmp.length];
+		for(int i = 0; i < tmp.length; i++)
+			resBin[i] = Character.getNumericValue(tmp[i]);
+
+		return convertToDecimal(resBin);
 	}
 
 
 	public static void main(String[] args){
 
-		LinkedList<String> diagnosticReport = readInput("testcase.txt");
-		long gammaRate = findGammaRate(diagnosticReport);
-		long epsilonRate = findEpsilonRate(diagnosticReport);
+		LinkedList<String> diagnosticReport = readInput("input.txt");
+
+		char[][] bits = convertToBits(diagnosticReport);		
+
+		long gammaRate = findGammaRate(bits);
+		long epsilonRate = findEpsilonRate(bits);
 		long powerConsumption = gammaRate * epsilonRate;
 		System.out.println("--- part one ---");
 		System.out.println("gamma rate: "+gammaRate);
 		System.out.println("epsilon rate: "+epsilonRate);
 		System.out.println("power consumption: "+powerConsumption);
 		System.out.println("--- part two ---");
-		
+		long oxigenGeneratorRating = findOxigenRating(bits);
+		long scrubberRating = findScrubberRating(bits);
+		long lifeSupportRating = oxigenGeneratorRating * scrubberRating;
+		System.out.println("oxigen generator rating: "+oxigenGeneratorRating);
+		System.out.println("CO2 scrubber rating: "+scrubberRating);
+		System.out.println("Life support rating: "+lifeSupportRating);
 	}
 
 }
